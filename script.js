2644a8e699e39b106b47f8e60e11e3361ce1189f
lasse333 <reneskjodt333@gmail.com>
2025-09-25 22:07:56 +0200

    Dag 2 - Parsning af dataen

--- script.js
+++ script.js
@@ -1,23 +1,107 @@
-const botcLobbies = [
+const botcLobbiesLinks = [
     "https://botc.app/join/clockmakers",
-    "https://botc.app/join/puppy's%20playground",
-    "https://botc.app/join/clockonmyblood",
 ];
 
 async function startup() {
+    let lobbies = [];
 
-    for (const lobby of botcLobbies) {
-
-        fetch(lobby)
-        .then(response => response.text())
-        .then(data => {
-            console.log(
-                parseHTMLRecursively(data),
-                getSelfClosingTags(data)
-            );
-        })
+    for (const lobby of botcLobbiesLinks) {
+        const botcLobby = new BOTCLobby(lobby);
+        lobbies.push(botcLobby);
     }
 
+    for (const lobby of lobbies) {
+        lobby.fetchLobbyData().then(() => {
+            console.log("Lobby URL:", lobby.url);
+            console.log("Is game running:", lobby.isGameRunning());
+            console.log("Game description:", lobby.getGameDescription());
+            console.log("Players:", lobby.getPlayers());
+        });
+    }
+
+}
+
+function isGameRunning(parsedHTML) {
+
+    return parsedHTML.getChildByTagName("body").content.length > 1;
+
+}
+
+function getGameDescription(parsedHTML) {
+
+    return parsedHTML.getChildByAttribute("property", "og:description").attributes.content;
+
+}
+
+class BOTCLobby {
+    #lobbyHTML;
+    #selfClosingTags;
+
+    constructor(url) {
+        this.url = url;
+    }
+
+    async fetchLobbyData() {
+        const response = await fetch(this.url);
+        const data = await response.text();
+        this.#lobbyHTML = parseHTMLRecursively(data)[0]; // Get the root HTML node
+        this.#selfClosingTags = new HTMLNode("html", {}, getSelfClosingTags(data));
+        
+    }
+
+    isGameRunning() {
+
+        return this.#lobbyHTML.getChildByTagName("body").content.length > 1;
+
+    }
+
+    getGameDescription() {
+        
+        if (!this.isGameRunning()) return "Game not running";
+
+        return this.#selfClosingTags.getChildByAttribute("property", "og:description").attributes.content;
+
+    }
+
+    getStoryTellers() {
+        if (!this.isGameRunning()) return [];
+
+        const storyTellers = [];
+        this.#lobbyHTML.getChildByTagName("body").getChildByAttribute("class", "storytellers").content.forEach(storyTellerNode => {
+            const storyTellerName = storyTellerNode.content;
+            storyTellers.push(storyTellerName);
+        });
+        return storyTellers;
+    }
+
+    getPlayers() {
+        if (!this.isGameRunning()) return [];
+
+        const players = [];
+        this.#lobbyHTML.getChildByTagName("body").getChildByAttribute("class", "players").content.forEach(playerNode => {
+            const playerName = playerNode.content;
+            players.push(playerName);
+        });
+        return players;
+    }
+}
+
+class HTMLNode {
+    constructor(tagName, attributes = {}, content = null) {
+        this.tagName = tagName;
+        this.attributes = attributes;
+        this.content = content; // Can be string or array of HTMLNode
+    }
+
+    getChildByTagName(tagName) {
+        if (!Array.isArray(this.content)) return null;
+        return this.content.find(child => child.tagName === tagName) || null;
+    }
+
+    getChildByAttribute(attrName, attrValue) {
+        if (!Array.isArray(this.content)) return null;
+        return this.content.find(child => child.attributes[attrName] === attrValue) || null;
+    }
 }
 
 function getSelfClosingTags(htmlString) {
@@ -29,11 +113,7 @@ function getSelfClosingTags(htmlString) {
         const tagName = match[1];
         const attributes = parseAttributes(match[2].trim());
 
-        selfClosingElements.push({
-            tagName,
-            attributes,
-            content: null
-        });
+        selfClosingElements.push(new HTMLNode(tagName, attributes));
     }
 
     return selfClosingElements;
@@ -53,11 +133,11 @@ function parseHTMLRecursively(htmlString) {
         // Recursively parse the content of the tag
         const children = parseHTMLRecursively(content);
 
-        elements.push({
+        elements.push(new HTMLNode(
             tagName,
             attributes,
-            content: children.length > 0 ? children : content
-        });
+            children.length > 0 ? children : content
+        ));
     }
 
     return elements;
